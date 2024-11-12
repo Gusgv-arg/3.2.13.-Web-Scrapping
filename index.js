a59f58ea4c59b1c46ec642e39c44c0952d8e81e9
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-11-12 10:48:26 -0300

    counter

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import {scrapperMercadoLibre2} from "./scrapperMercadoLibre2.js"
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-
+let contador = 1
 app.get("/scrape/mercado_libre", async (req, res) => {
-  console.log("Api de Scrapping llamada!!")
+  console.log("Api de Scrapping llamada numero:", contador)
+  contador ++
   console.log("req:", req)
   const allProducts = await scrapperMercadoLibre2();
   res.status(200).send(allProducts)
