af35fa02b690380ab50d19591a4f97a4a56b14db
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-11-13 17:21:57 -0300

    url

--- scrapperMercadoLibre2.js
+++ scrapperMercadoLibre2.js
@@ -30,15 +30,15 @@ export const scrapperMercadoLibre2 = async () => {
 	});
 	const page = await browser.newPage();
 
-	/* const urls = [
+	const urls = [
 		"https://motos.mercadolibre.com.ar/scooters/motomel/blitz-110/blitz110_KILOMETERS_0km-0km_NoIndex_True#applied_filter_id%3DKILOMETERS%26applied_filter_name%3DKil%C3%B3metros%26applied_filter_order%3D8%26applied_value_id%3D%5B0km-0km%5D%26applied_value_name%3D0+km%26applied_value_order%3D2%26applied_value_results%3D94%26is_custom%3Dfalse",
 		"https://motos.mercadolibre.com.ar/naked/benelli/leoncino-500_ITEM*CONDITION_2230284_NoIndex_True#applied_filter_id%3DITEM_CONDITION%26applied_filter_name%3DCondici%C3%B3n%26applied_filter_order%3D9%26applied_value_id%3D2230284%26applied_value_name%3DNuevo%26applied_value_order%3D1%26applied_value_results%3D47%26is_custom%3Dfalse",
 		"https://motos.mercadolibre.com.ar/naked/suzuki/ax-100/ax100_ITEM*CONDITION_2230284_NoIndex_True#applied_filter_id%3DITEM_CONDITION%26applied_filter_name%3DCondici%C3%B3n%26applied_filter_order%3D6%26applied_value_id%3D2230284%26applied_value_name%3DNuevo%26applied_value_order%3D1%26applied_value_results%3D44%26is_custom%3Dfalse",
 		"https://motos.mercadolibre.com.ar/scooters/motomel/strato-150_ITEM*CONDITION_2230284_NoIndex_True#applied_filter_id%3DITEM_CONDITION%26applied_filter_name%3DCondici%C3%B3n%26applied_filter_order%3D8%26applied_value_id%3D2230284%26applied_value_name%3DNuevo%26applied_value_order%3D1%26applied_value_results%3D40%26is_custom%3Dfalse"
-	]; */
-	const urls = [
+	]; 
+	/* const urls = [
 		"https://motos.mercadolibre.com.ar/calle/benelli/leoncino-500_ITEM*CONDITION_2230284_NoIndex_True#applied_filter_id%3DMOTO_TYPE%26applied_filter_name%3DTipo+de+moto%26applied_filter_order%3D4%26applied_value_id%3D2343326%26applied_value_name%3DStreet%26applied_value_order%3D3%26applied_value_results%3D2%26is_custom%3Dfalse"
-	];
+	]; */
 
 	let allProducts = [];
 
