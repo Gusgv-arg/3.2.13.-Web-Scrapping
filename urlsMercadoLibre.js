40ff77f559b80da1d7ec45624803860e0b439381
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-12-02 11:27:29 -0300

    url

--- urlsMercadoLibre.js
+++ urlsMercadoLibre.js
@@ -1,10 +1,7 @@
 export const urlsMercadoLibre = [
-	"https://motos.mercadolibre.com.ar/2024/motomel-blitz-110_Desde_145_OrderId_PRICE_KILOMETERS_0km-0km_NoIndex_True",
-	"https://motos.mercadolibre.com.ar/motomel/blitz-110/concesionaria/motomel-blitz-110_ITEM*CONDITION_2230284_NoIndex_True#applied_filter_id%3DMODEL%26applied_filter_name%3DModelo%26applied_filter_order%3D3%26applied_value_id%3D1218638%26applied_value_name%3DBlitz+110%26applied_value_order%3D2%26applied_value_results%3D127%26is_custom%3Dfalse",
+	"https://motos.mercadolibre.com.ar/benelli/benelli-motos_Desde_721_ITEM*CONDITION_2230284_NoIndex_True",
 ];
-/* export const urlsMercadoLibre = [
-    "https://motos.mercadolibre.com.ar/scooters/motomel/blitz-110/blitz110_KILOMETERS_0km-0km_NoIndex_True#applied_filter_id%3DKILOMETERS%26applied_filter_name%3DKil%C3%B3metros%26applied_filter_order%3D8%26applied_value_id%3D%5B0km-0km%5D%26applied_value_name%3D0+km%26applied_value_order%3D2%26applied_value_results%3D94%26is_custom%3Dfalse",
-    "https://motos.mercadolibre.com.ar/naked/benelli/leoncino-500_ITEM*CONDITION_2230284_NoIndex_True#applied_filter_id%3DITEM_CONDITION%26applied_filter_name%3DCondici%C3%B3n%26applied_filter_order%3D9%26applied_value_id%3D2230284%26applied_value_name%3DNuevo%26applied_value_order%3D1%26applied_value_results%3D47%26is_custom%3Dfalse",
-    "https://motos.mercadolibre.com.ar/naked/suzuki/ax-100/ax100_ITEM*CONDITION_2230284_NoIndex_True#applied_filter_id%3DITEM_CONDITION%26applied_filter_name%3DCondici%C3%B3n%26applied_filter_order%3D6%26applied_value_id%3D2230284%26applied_value_name%3DNuevo%26applied_value_order%3D1%26applied_value_results%3D44%26is_custom%3Dfalse",
-    "https://motos.mercadolibre.com.ar/scooters/motomel/strato-150_ITEM*CONDITION_2230284_NoIndex_True#applied_filter_id%3DITEM_CONDITION%26applied_filter_name%3DCondici%C3%B3n%26applied_filter_order%3D8%26applied_value_id%3D2230284%26applied_value_name%3DNuevo%26applied_value_order%3D1%26applied_value_results%3D40%26is_custom%3Dfalse"
-]; */
+
+const urlChequeadas = [
+    {modelo: "Blitz 110", url: "https://motos.mercadolibre.com.ar/motomel/blitz-110/concesionaria/motomel-blitz-110_ITEM*CONDITION_2230284_NoIndex_True#applied_filter_id%3DMODEL%26applied_filter_name%3DModelo%26applied_filter_order%3D3%26applied_value_id%3D1218638%26applied_value_name%3DBlitz+110%26applied_value_order%3D2%26applied_value_results%3D127%26is_custom%3Dfalse" }
+]
