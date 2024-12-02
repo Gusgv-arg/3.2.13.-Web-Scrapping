59681b98af95b283ebce5a1763bf2b5daffb858f
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-12-02 15:32:41 -0300

    180

--- urlsMercadoLibre.js
+++ urlsMercadoLibre.js
@@ -1,7 +1,9 @@
 export const urlsMercadoLibre = [
-	"https://motos.mercadolibre.com.ar/benelli/benelli-motos_Desde_721_ITEM*CONDITION_2230284_NoIndex_True",
+	"https://motos.mercadolibre.com.ar/benelli/concesionaria/benelli-180-s_Desde_49_NoIndex_True",
 ];
 
 const urlChequeadas = [
-    {modelo: "Blitz 110", url: "https://motos.mercadolibre.com.ar/motomel/blitz-110/concesionaria/motomel-blitz-110_ITEM*CONDITION_2230284_NoIndex_True#applied_filter_id%3DMODEL%26applied_filter_name%3DModelo%26applied_filter_order%3D3%26applied_value_id%3D1218638%26applied_value_name%3DBlitz+110%26applied_value_order%3D2%26applied_value_results%3D127%26is_custom%3Dfalse" }
+    "https://motos.mercadolibre.com.ar/motomel/blitz-110/concesionaria/motomel-blitz-110_ITEM*CONDITION_2230284_NoIndex_True#applied_filter_id%3DMODEL%26applied_filter_name%3DModelo%26applied_filter_order%3D3%26applied_value_id%3D1218638%26applied_value_name%3DBlitz+110%26applied_value_order%3D2%26applied_value_results%3D127%26is_custom%3Dfalse",
+    "https://motos.mercadolibre.com.ar/benelli/concesionaria/benelli-180-s_Desde_49_NoIndex_True" 
+    
 ]
