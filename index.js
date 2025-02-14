e4913d40046cc33c39244f02b23565bee0f8ddff
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2025-02-14 10:47:48 -0300

    msje

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 let contador = 1
 app.get("/scrape/mercado_libre", async (req, res) => {
-  console.log("Api de Scrapping Mercado Libre:", contador)
+  console.log("Api de Scrape Mercado Libre:", contador)
   contador ++
   const allProducts = await scrapperMercadoLibre();
   res.status(200).send(allProducts)
 });
 
 app.get("/scrape/facebook", async (req, res) => {
-  console.log("Api de Scrapping Facebook:", contador)
+  console.log("Api de Scrape Facebook:", contador)
   contador ++
   const allProducts = await apifyFacebookScraper();
   console.log("Allproducts:", allProducts)
