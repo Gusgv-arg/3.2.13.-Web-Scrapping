f4247d75e213845cebe21277124c2ddc53341dae
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2025-02-17 12:17:24 -0300

    url

--- urlsMercadoLibre.js
+++ urlsMercadoLibre.js
@@ -5,7 +5,7 @@
 	}
 ] */   
 
-export const urlsMercadoLibre = [
+/* export const urlsMercadoLibre = [
 	{
 		url: "https://motos.mercadolibre.com.ar/scooters/motomel/blitz-110/concesionaria/motomel-blitz-110_ITEM*CONDITION_2230284_NoIndex_True",
 		palabraClave: ["Blitz"],
@@ -98,9 +98,9 @@ export const urlsMercadoLibre = [
 		url: "https://motos.mercadolibre.com.ar/motomel/strato/concesionaria/motomel-strato_ITEM*CONDITION_2230284_NoIndex_True",
 		palabraClave: ["Strato"],
 	},
-]
+] */
 
-/* export const urlsMercadoLibre = [
+export const urlsMercadoLibre = [
 	{
 		url: "https://motos.mercadolibre.com.ar/motomel/concesionaria/motomel-xmm_ITEM*CONDITION_2230284_NoIndex_True",
 		palabraClave: ["Xmm"],
@@ -181,7 +181,7 @@ export const urlsMercadoLibre = [
 		url: "https://motos.mercadolibre.com.ar/concesionaria/rerace-teknial_BRAND_5411085_ITEM*CONDITION_2230284_NoIndex_True",
 		palabraClave: ["Rerace", "Re race"],
 	}
-]; */   
+];   
 /* const urls = ()=>{
 	console.log(urlsMercadoLibre.length)
 
